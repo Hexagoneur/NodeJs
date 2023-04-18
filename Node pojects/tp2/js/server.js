7d19fbd8b8f71c0b5ba41cf9850d56aa30d1706b
Hexagoneur <115691284+Hexagoneur@users.noreply.github.com>
2023-04-19 00:46:54 +0200

    Update server.js

--- Node pojects/tp2/js/server.js
+++ Node pojects/tp2/js/server.js
@@ -1,112 +1,93 @@
-function salon(id_utilisateur_dest) {
-  clearMessages(); // Effacement du contenu de la zone de messages
-  socket.join(id_utilisateur_dest);
-  socket.emit('changement_salon', id_utilisateur_dest); // Demande au serveur de changer de salon
-
-  document.getElementById('titre').innerHTML = 'Salon ' + id_salon; // Modification du titre de la page
-  document.getElementById('input').value = ''; // Effacement du champ d'entrée de message
-}
-
-function clearMessages() {
-  var messageContainer = document.getElementById('message-container');
-  messageContainer.innerHTML = ''; // Suppression des éléments dans le conteneur de messages
-}
-
-// Configuration du serveur et des modules
-
-const express = require('express');
+//COnfiguration du serv et des modules
+const express =require('express');
 const app = express();
 const http = require('http');
-const server = http.createServer(app);
-const { Server } = require('socket.io');
-const io = new Server(server);
-var path = require('path');
-let PORT = 8080;
-const salons = {};
+const server =http.createServer(app);
+const {Server} = require("socket.io");
+const io =new Server(server);
+var path = require("path");
+let PORT =8080;
 
 
-// Port d'écoute
-server.listen(PORT, () => {
-  console.log('Serveur démarré sur le Port', +PORT);
-});
 
-// Direction route page d'accueil
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
-});
-
-// Direction route page css
-app.get('/css/style.css', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '/css/style.css'));
-});
-
-// Direction route page client
-app.get('/js/client.js', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', '/js/client.js'));
-});
-
-// Gestion des connexions des clients
-io.on('connection', (socket) => {
-  socket.join('salon');
-  socket.on('set-pseudo', (pseudo) => {
-    console.log(pseudo + ' vient de se connecter à ' + new Date());
-
-    socket.nickname = pseudo;
-
-    //Permet de récupérer la liste des clients connectés
-    io.fetchSockets().then((room) => {
-      var utilisateurs = [];
-      room.forEach((item) => {
-        utilisateurs.push({
-          id_client: item.id,
-          pseudo_client: item.nickname,
-        });
-      });
-      console.table(utilisateurs);
-      io.emit('reception_utilisateurs', utilisateurs);
-    });
-  });
-
-  socket.on('emission_message', (message) => {
-    console.log(socket.nickname + ':' + message);
-
-    //socket.broadcast.emit(socket.nickame+':'+message);
-    io.emit('reception_message', message);
-  });
-
- // Création d'une room privée entre deux utilisateurs
- socket.on('create_room', (data) => {
-  const { emmet_id, dest_id } = data;
-
-  // Vérification de la présence des deux utilisateurs dans la liste des utilisateurs connectés
-  if (!utilisateurs[emmet_id] || !utilisateurs[dest_id]) {
-    return;
+function salon(id_utilisateur_dest) {
+    clearMessages(); // Effacement du contenu de la zone de messages
+    socket.join(id_utilisateur_dest);
+    socket.emit('changement_salon', id_utilisateur_dest); // Demande au serveur de changer de salon
+  
+    document.getElementById('titre').innerHTML = 'Salon ' + id_salon; // Modification du titre de la page
+    document.getElementById('input').value = ''; // Effacement du champ d'entrée de message
   }
-
-      // Création d'une room unique pour les deux utilisateurs
-      const roomName = `${emmet_id}_${dest_id}`;
-      socket.join(roomName);
-      console.log(`Room ${roomName} créée`);
   
-      // Envoi d'un message aux deux utilisateurs pour les informer de la création de la room
-      io.to(utilisateurs[emmet_id]).to(utilisateurs[dest_id]).emit('room_created', { roomName });
+  function clearMessages() {
+    var messageContainer = document.getElementById('message-container');
+    messageContainer.innerHTML = ''; // Suppression des éléments dans le conteneur de messages
+  }
+  
+
+  
+  // Port d'écoute
+  server.listen(PORT, () => {
+    console.log('Serveur démarré sur le Port', +PORT);
+  });
+  
+  // Direction route page d'accueil
+  app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'index.html'));
+  });
+  
+  // Direction route page css
+  app.get('/css/style.css', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/css/style.css'));
+  });
+  
+  // Direction route page client
+  app.get('/js/client.js', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', '/js/client.js'));
+  });
+  
+  // Gestion des connexions des clients
+  io.on('connection', (socket) => {
+    socket.join('salon');
+    socket.on('set-pseudo', (pseudo) => {
+      console.log(pseudo + ' vient de se connecter à ' + new Date());
+  
+      socket.nickname = pseudo;
+  
+      //Permet de récupérer la liste des clients connectés
+      io.fetchSockets().then((room) => {
+        var utilisateurs = [];
+        room.forEach((item) => {
+          utilisateurs.push({
+            id_client: item.id,
+            pseudo_client: item.nickname,
+          });
+        });
+        console.table(utilisateurs);
+        io.emit('reception_utilisateurs', utilisateurs);
+      });
     });
   
-
-
-
-  socket.on('emission_privé', (message) => {
-    console.log(socket.nickname);
-    io.to(message.emet_id).to(message.dest_id).emit('reception_message', message);
+    socket.on('emission_message', (message) => {
+      console.log(socket.nickname + ':' + message);
+  
+      //socket.broadcast.emit(socket.nickame+':'+message);
+      io.emit('reception_message', message);
+    });
+  
+    socket.on('emission_privé', (message) => {
+      console.log(socket.nickname);
+      io.to(message.emet_id).to(message.dest_id).emit('reception_message', message);
+    });
+  
+  
+  //io.to(id_utilisateur_dest).emit('nouvel_utilisateur', socket.nickname);
+  
+    //Permet d'afficher les utilisateurs qui se déconnectent
+    socket.on('disconnect', () => {
+      console.log(socket.nickname + ' vient de se déconnecter à ' + new Date());
+      const pseudo = socket.nickname;
+      socket.broadcast.emit('left_user', { pseudo });
+    });
   });
-
-
-//io.to(id_utilisateur_dest).emit('nouvel_utilisateur', socket.nickname);
-
-  //Permet d'afficher les utilisateurs qui se déconnectent
-  socket.on('disconnect', () => {
-    console.log(socket.nickname + ' vient de se déconnecter à ' + new Date());
-    const pseudo = socket.nickname;
-    socket.broadcast.emit('left_user', { pseudo });
-  });
-});
+  
